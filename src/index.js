d9a26d4c33be039e695e0825e2c065bb6d21289c
Peter Lee <xianrenb@gmail.com>
2019-12-27 11:46:13 +0800

    change to use MonoSynth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Tone = require("tone");
 
 function Mk20edo(){
   //create a synth and connect it to the master output (your speakers)
-  const synth = new Tone.Synth().toMaster();
+  const synth = new Tone.MonoSynth().toMaster();
 
   const lut = "zZxXcCvVbBnNmM,<.>/?" +
               "aAsSdDfFgGhHjJkKlL;:" +
@@ -11,10 +11,13 @@ function Mk20edo(){
   const lut2 = "123456789";
 
   const s = Math.log(2.0)/20;
-  const zFreq = 130.8128;
+  const s12 = Math.log(2.0)/12;
+  const c0Freq = Math.exp(s12 * -57) * 440.0;
+  zFreq = c0Freq * 8;
   this.lut = lut;
   this.lut2 = lut2;
   this.s = s;
+  this.c0Freq = c0Freq;
   this.zFreq = zFreq;
   this.synth = synth;
 };
@@ -26,15 +29,18 @@ Mk20edo.prototype = {
 
     if (i >= 0){
       const freq = Math.exp(i * this.s) * this.zFreq;
-      this.synth.triggerAttackRelease(freq, "8n");
+      this.synth.triggerAttack(freq);
       return;
     }
 
     i = this.lut2.indexOf(event.key);
 
     if (i >= 0){
-      this.zFreq = Math.exp(i * 20 * this.s) * 16.35160;
+      this.zFreq = Math.exp(i * 20 * this.s) * this.c0Freq;
     }
+  },
+  KeyUp: function(event){
+    this.synth.triggerRelease();
   }
 };
 
@@ -49,6 +55,7 @@ $(function(){
 
   $( "#in" ).keyup(function(event) {
      event.preventDefault();
+     mk20edo.KeyUp(event);
      $( "#in" ).val("_");
   });
 });
